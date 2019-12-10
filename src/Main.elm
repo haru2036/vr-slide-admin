d52117885ba0258c9fc275e91d516e1bd93cfb6f
haru2036 <haru2036@gmail.com>
2019-12-10 12:56:54 +0900

    イベント編集途中まで

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,9 @@ port module Main exposing (Model, Msg(..), toJs, init, main, update, view, signe
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
+import List.Extra exposing (setAt)
+import String exposing(fromInt, toInt)
 import Url
 import Url.Builder as URLB
 import Url.Parser as URLP exposing((</>))
@@ -68,6 +70,7 @@ type Route
   | Signin 
   | EventsList
   | EventDetail String
+  | EventModify String
 
 route : URLP.Parser (Route -> a) a
 route =
@@ -76,6 +79,7 @@ route =
     , URLP.map Signin (URLP.s "")
     , URLP.map EventsList (URLP.s "events")
     , URLP.map EventDetail (URLP.s "event" </> URLP.string)
+    , URLP.map EventModify (URLP.s "event" </> URLP.s "edit" </> URLP.string)
     ]
 -- ---------------------------
 -- UPDATE
@@ -90,6 +94,12 @@ type Msg
     | LinkClicked Browser.UrlRequest
     | GotEvents (Result Http.Error (List Event))
     | GotEvent (Result Http.Error Event)
+    | EventModified ModifyAction
+    | NoOp
+
+type ModifyAction = Swap Int Int
+                  | ChangeSlide Int Slide
+                  | AddSlide
 
 type Page = Login
           | Events
@@ -98,6 +108,7 @@ type Page = Login
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
+        NoOp -> (model, Cmd.none)
         SignedIn token ->
             ({ model | idToken = token}, Nav.pushUrl model.key "/events")
         SignIn -> (model, signIn ())
@@ -121,11 +132,29 @@ update message model =
             case (URLP.parse route url) of 
                 Just EventsList -> ( { model | url = url }, loadEvents model.idToken)
                 Just (EventDetail name) -> ( {model | url = url}, loadEvent model.idToken name)
+                Just (EventModify name) -> ( {model | url = url}, loadEvent model.idToken name)
                 _ -> ( { model | url = url }, Cmd.none )
         LinkClicked urlRequest -> 
             case urlRequest of
                 Browser.Internal url -> (model, Nav.pushUrl model.key (Url.toString url) )
                 Browser.External href -> ( model, Nav.load href )
+        EventModified modifyAction ->
+            case model.currentEvent of
+                Just event ->
+                    case modifyAction of
+                        Swap a b -> ( model, Cmd.none) --todo
+                        ChangeSlide idx slide -> 
+                            let
+                                oldCurrentEvent = event
+                                newCurrentEvent = Just { oldCurrentEvent | slides = setAt idx slide event.slides }
+                            in ({model | currentEvent = newCurrentEvent}, Cmd.none)
+                        AddSlide -> 
+                            let
+                                oldCurrentEvent = event
+                                newCurrentEvent = Just ( { oldCurrentEvent | slides = Slide "" 0 :: event.slides })
+                            in ({model | currentEvent = newCurrentEvent}, Cmd.none)
+                Nothing -> ( model, Cmd.none ) 
+
 
 loadEvents : String -> Cmd Msg
 loadEvents idToken = Http.request
@@ -174,6 +203,22 @@ httpErrorToString err =
 -- VIEW
 -- ---------------------------
 
+eventEditView : Model -> Html Msg
+eventEditView model = case model.currentEvent of
+                Just event -> div [] [text event.name, button [ class "pure-button pure-button-primary"
+                                    , onClick <| EventModified AddSlide ] [text "Add"]
+                                    , event.slides |> List.indexedMap slideEditRow |> div []
+                                        ]
+                Nothing -> button [ class "pure-button pure-button-primary"
+                                    , onClick <| EventModified AddSlide ] [text "Add"]
+
+slideEditRow : Int -> Slide -> Html Msg
+slideEditRow index slide = ul [] [ input [onInput (\newSdid -> EventModified (ChangeSlide index {slide | sdid = newSdid })) , value slide.sdid ] []
+                                , input [type_ "number", onInput (\newCount -> case toInt newCount of
+                                                                                Just count -> EventModified (ChangeSlide index {slide | count = count })
+                                                                                Nothing -> NoOp
+                                                                    ) , value <| fromInt slide.count] []  ] 
+
 eventListView : Model -> Html Msg
 eventListView model = case model.events of
     [] -> div [class "container"] [text "empty"
@@ -182,20 +227,21 @@ eventListView model = case model.events of
                         ]
                         [ text "Reload" ]
                         ]
-    _ -> model.events |> List.map (\ev -> div [ class "container" ] [a [href <| URLB.relative ["event", ev.name] []] [text ev.name], div [] [(eventView ev.slides)]]) |> div []
+    _ -> model.events |> List.map (\ev -> div [ class "container" ] [a [href <| URLB.absolute ["event", ev.name] []] [text ev.name], div [] [(slideListView ev.slides)]]) |> div []
 
 eventDetailView : Model -> Html Msg 
 eventDetailView model = case model.currentEvent of 
-    Just event -> div [ class "container" ] [text event.name, div [] [(eventView event.slides)]]
+    Just event -> div [ class "container" ] [text event.name, a [href <| URLB.absolute ["event", "edit", event.name] []] [text "edit"], div [] [(slideListView event.slides)]]
     Nothing -> text "empty"
 
-eventView : List Slide -> Html Msg
-eventView slides = slides |> List.map (\s -> ul [] [text s.sdid] ) |> div []
+slideListView : List Slide -> Html Msg
+slideListView slides = slides |> List.map (\s -> ul [] [text s.sdid] ) |> div []
 
 view : Model -> Html Msg
 view model = case (model.idToken, URLP.parse route model.url) of
     (_, Just EventsList) -> eventListView model
     (_, Just (EventDetail eventName)) -> eventDetailView model
+    (_, Just (EventModify eventName)) -> eventEditView model
     (_, _) ->
         div [ class "container" ]
             [ header []
